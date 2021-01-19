592f143d419b836fef15b227d3a301a4eaac9fa4
MadelineHenry <madelinehenry04101996@gmail.com>
2021-01-19 17:15:44 +0100

    Login page html + css base

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,4 @@
 <?php
-echo "ah que coucou";
 // login("jean@coucou.be", "coucou")
 ?>
 
@@ -9,7 +8,7 @@ echo "ah que coucou";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css">
     <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
 
     <title>Breaking Badge</title>
@@ -18,9 +17,19 @@ echo "ah que coucou";
 <body>
     <div class="login_container">
         <form method="post">
-            <input for="email" type="email" name="email" placeholder="Email"></input>
-            <input for="password" type="password" name="password" placeholder="Password">Password :</input>
-            <button class="login_button" type="submit">Log In</button>
+            <div class="titles">
+                <h1 class="welcome_title">Welcome</h1>
+                <h2 class="BB_title">To Breaking Badges</h2>
+            </div>
+            <div class="email_input">
+                <input for="email" type="email" name="email" placeholder="Email"></input>
+            </div>
+            <div class="psw_input">
+                <input for="password" type="password" name="password" placeholder="Password"></input>
+            </div>
+            <div class="submit_input">
+                <button class="login_button" type="submit">Log In</button>
+            </div>
         </form>
     </div>
 </body>
