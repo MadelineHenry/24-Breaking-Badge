c6522b434a5db6e754f00044dcd51aff3e225ed7
JeanFabry <jeanfabry1435@gmail.com>
2021-01-20 10:54:44 +0100

    Badges for one user

--- pages/badges.php
+++ pages/badges.php
@@ -1,21 +1,35 @@
-<?php 
+<?php
+//JEAN
+include("../components/functions.php");
+//END JEAN
 
- if($_SESSION['account_type'] === 'ADMIN'){
-     
+if ($_SESSION['account_type'] === 'ADMIN') {
+
     include_once('navbarAdmin.php');
+} else {
 
- }
- else {
-
- include("navbarNormies.php");
-
+    include("navbarNormies.php");
 }
 
-if($_SESSION['account_type'] === 'NORMIES'){
+if ($_SESSION['account_type'] === 'NORMIES') {
     /* my badges */
 }
 
-include("../components/allBadgesContent.php");
+//JEAN
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
- ?>
+<body>
+    <div class=badgesOneUser style="display: flex;"><?= getUserBadges(); ?></div>
+    <div><?= getBadges(); ?> </div>
+</body>
+
+</html>
+<!-- //END JEAN -->
