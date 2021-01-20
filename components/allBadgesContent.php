bca64bd9c641a7995fbfddc3b30162afd5d9cf46
JeanFabry <jeanfabry1435@gmail.com>
2021-01-20 09:27:39 +0100

    All badges done

--- components/allBadgesContent.php
+++ components/allBadgesContent.php
@@ -1,17 +1,7 @@
 <?php
 //JEAN
 include("../components/db.php");
-$bdd = createCursor();
-$requestAllBadges = $bdd->query("SELECT name_badge, description_badge, color_badge FROM badges");
+include("../components/functions.php");
 
-while ($answerOneBadge = $requestAllBadges->fetch()) {
-echo "<div class='allBadges' style='display:flex'>
-    <div class='badge' style='display:flex;justify-content:center;align-items:center;height:100px;width:100px;border-radius:50%;background-color:".$answerOneBadge['color_badge']."'>"
-    .$answerOneBadge['name_badge'].
-    "</div>
-    <div style='display:flex;justify-content:center;align-items:center;'>"
-    .$answerOneBadge['description_badge'].
-    "</div>
-</div>";
-};
+getBadges();
 //Jean
--- components/functions.php
+++ components/functions.php
@@ -45,8 +45,21 @@
     session_destroy();
   }
 
+  //JEAN
   function getBadges(){
+  $bdd = createCursor();
+  $requestAllBadges = $bdd->query("SELECT name_badge, description_badge, color_badge FROM badges");
 
+  while ($answerOneBadge = $requestAllBadges->fetch()) {
+    echo "<div class='allBadges' style='display:flex'>
+    <div class='badge' style='display:flex;justify-content:center;align-items:center;height:100px;width:100px;border-radius:50%;background-color:" . $answerOneBadge['color_badge'] . "'>"
+    . $answerOneBadge['name_badge'] .
+      "</div>
+    <div style='display:flex;justify-content:center;align-items:center;'>"
+      . $answerOneBadge['description_badge'] .
+      "</div>
+</div>";
+  };
   }
 
   function getUsers(){
