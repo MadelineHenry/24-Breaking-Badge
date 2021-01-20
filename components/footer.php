671ec70b2e703f50f1f00ad50d724a86febf0c11
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-20 17:11:50 +0100

    Finition du mercredi

---
+++ components/footer.php
@@ -0,0 +1,13 @@
+<!-- Bas de page -->
+
+<footer class="site-footer">
+<div class="footer">
+    <p>Fair use disclaimer, this website is for educational purpose only.</p>
+    <p class="copyright_footer">&copy copyright - BeCode - </p>
+    <p>- <a href="https://github.com/MadelineHenry">Madeline</a> - 
+    <a href="https://github.com/Cemil1000">Cemil</a> - 
+    <a href="https://github.com/JeanFabry">Jean</a> - 
+    <a href="https://github.com/IgorDeSpi">Igor</a> -</p>
+</div>
+
+</footer>
