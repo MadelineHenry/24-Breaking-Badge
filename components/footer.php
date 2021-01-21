032a521867c2935fcff21d9d36f546e6fca18799
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-21 10:11:59 +0100

    Changement CSS + ajout footer

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,20 @@
 <!-- Bas de page -->
 
 <footer class="site-footer">
-<div class="footer">
-    <p>Fair use disclaimer, this website is for educational purpose only.</p>
-    <p class="copyright_footer">&copy copyright - BeCode - </p>
-    <p>- <a href="https://github.com/MadelineHenry">Madeline</a> - 
-    <a href="https://github.com/Cemil1000">Cemil</a> - 
-    <a href="https://github.com/JeanFabry">Jean</a> - 
-    <a href="https://github.com/IgorDeSpi">Igor</a> -</p>
-</div>
+    <div class="footer">
+        <p>Fair use disclaimer, this website is for educational purpose only.</p>
+        <div class="centrage_liens">
+            <div class='liens'>
+                <p class="copyright_footer">&copy copyright - BeCode</p>
+                <p class='liens_github'>-
+                    <a class='liens_footer' target="_blank" href="https://github.com/MadelineHenry">Madeline</a> -
+                    <a class='liens_footer' target="_blank" href="https://github.com/Cemil1000">Cemil</a> -
+                    <a class='liens_footer' target="_blank" href="https://github.com/JeanFabry">Jean</a> -
+                    <a class='liens_footer' target="_blank" href="https://github.com/IgorDeSpi">Igor</a> -
+                </p>
+                <a class='no_margin_bottom liens_footer' target="_blank" href="https://github.com/MadelineHenry/24-Breaking-Badge">Our repository <i title='Our repository'class="fab fa-github"></i></a>
+            </div>
+        </div>
+    </div>
 
 </footer>
